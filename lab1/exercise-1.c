7cc9e77e43de9a1935313b6091506f8b3631c0d0
RubenTadeia <rubent03@gmail.com>
2022-11-21 21:31:44 +0000

    Falta ordenar vector

--- lab1/exercise-1.c
+++ lab1/exercise-1.c
@@ -1,17 +1,68 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(){
-	char line[100];
-	int n;
-	printf("What is your student number?\n");
-	fgets(line, 100, stdin);
-	if (sscanf(line, "%d", &n) != 1){
-		printf("invalid Number\n");
+// Variables
+char ** word_array;
+int n_words;
+int max_word_len;
+char inputFile[30] = "lusiadas-words.txt";
+
+// Functions
+void read_words(char * fname){
+
+	char linha[100];
+	FILE * file = fopen(fname, "r");
+	if(file == NULL){
+		perror("Opening file");
 		exit(-1);
 	}
-	srandom(n);
-	printf("your own random %ld\n", random());   
+	n_words =0;
+	max_word_len = 0;
+	while(fgets(linha, 100, file)){
+		if(linha[0] != '\n' &&  linha[0] != '\0' && isalpha(linha[0])){
+			//The isalpha() function checks whether a character is an alphabet or not.
+			n_words++;
+			if(strlen(linha)-1 > max_word_len){
+				max_word_len = strlen(linha)-1 ;
+			}
+		}   
+	}
+	printf("%d numero de palavras\n", n_words);
+	word_array = calloc(sizeof(char *), n_words);
+	if (word_array == NULL){
+		perror("malloc ");
+		exit(0);
+	}
+	max_word_len = max_word_len +1;
+	file = fopen(fname, "r");
+	if(file == NULL){
+		perror("opening file");
+		exit(-1);
+	}
+	int i = 0;
+	while(fgets(linha, 100, file)){
+		if(linha[0] != '\n' && linha[0] != '\0' && isalpha(linha[0])){
+		   linha[strlen(linha)-1] = '\0';
+		   for (int j = 0; linha[j]!= '\0'; j++){
+			   linha[j] = tolower(linha[j]);
+		   } 
+			word_array[i] = calloc(sizeof(char), max_word_len);
+			strcpy(word_array[i], linha);
+			i++;
+		}
+	}
+	fclose(file);
+}
+
+// Main
+int main(){
+	printf("Reading the file. Please note that empty lines will be ignored\n");
+	read_words(inputFile);
+
+	for (int i = 0; i < n_words ; i++){
+		printf("%s\n", word_array[i]);
+	}
 	exit(0);
 }
