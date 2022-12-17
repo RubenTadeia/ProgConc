4042e05391492e382ee174d5962ebb17fc333a4c
RubenTadeia <rubent03@gmail.com>
2022-12-17 01:02:23 +0000

    Lab 5 - Solved Exercise 1?

--- lab5/lab5-pipe-example.c
+++ lab5/lab5-pipe-example.c
@@ -40,18 +40,17 @@ int main(){
 		exit(-1);
 	}
 	int n = 0;
-	int thread_index = 1;
+	int number_of_threads = 4;
 	//int read_value;
 	// infinite loop that in each iteration writes a number to the pipe and read it afterwards
 	// Loop infinite que em cada iterecao escreve na pipe e le de seguida
 	while(1){
 		// Variaveis de Threads
-		pthread_t thread_id;
-		thread_input_info * thread_information = (thread_input_info *) malloc (sizeof(thread_input_info));
+		int thread_index = 1;
 
 		// Inicio do loop
 		printf("going to write %d into pipe_fd[1]\n", n);
-		n++;
+		//n++;
 		//sleep(1);
 		
 		write(pipe_fd[1], &n, sizeof(n));
@@ -60,17 +59,21 @@ int main(){
 		// &n is a pointer to the data to be written
 		// sizeof(n) is the amount of data to write in the pipe
 
-		thread_information->thread_id = thread_index;
-		thread_information->pipe_function = pipe_fd[0];
-		pthread_create(&thread_id, NULL, thread_function, thread_information);
+		for (int iterator = 0; iterator < number_of_threads; iterator++){
+			pthread_t thread_id;
+			thread_input_info * thread_information = (thread_input_info *) malloc (sizeof(thread_input_info));
+			thread_information->thread_id = thread_index;
+			thread_information->pipe_function = pipe_fd[0];
+			pthread_create(&thread_id, NULL, thread_function, thread_information);
+			thread_index++;
+		}
+		
 		//read(pipe_fd[0], &read_value, sizeof(read_value));
 
 		// pipe_fd[0] correpsonded to the exit of data in the pipe
 		// &n is apointer to variable that will hold the read data
 		// sizeof(n) is the ammount of data to be read from the pipe
 		// printf("\tJust read %d from pipe_fd[0]\n",read_value);
-		//n++;
-		thread_index++;
+		n++;
 	}
-
 }
